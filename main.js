3124d7cb0a1bbc6fffe098fa206a24d12d538eaf
Zamir31 <noriegad258@gmail.com>
2023-03-08 01:54:13 -0600

    Galeria de imagenes y videos terminado

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-class Escuela {
-    title = 'SIGUE TUS ESTUDIOS CON NOSOTROS';
+mostrarImagenes = () => {
+    document.getElementById("GaleriaVideos").style.display = "none";
+    document.getElementById("galeriaImagenes").removeAttribute("style");
+    document.getElementById("videos").removeAttribute("class");
+    document.getElementById("imagenes").setAttribute('class', 'active');
+}
+
+mostrarVideos = () => {
+    document.getElementById("galeriaImagenes").style.display = "none";
+    document.getElementById("GaleriaVideos").removeAttribute("style");
+    document.getElementById("imagenes").removeAttribute("class");
+    document.getElementById("videos").setAttribute('class', 'active');
 }
